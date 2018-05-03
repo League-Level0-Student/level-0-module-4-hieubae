61b8c01aa35157889d2452c95a58b79b33f5c7f2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-03 13:25:51 -0700

    Choose Your Own Adventure almost done

---
+++ src/ChooseYourOwnAdventure.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class ChooseYourOwnAdventure {
+public static void main(String[] args) {
+	
+	
+	String[] options = {"Who are you?", "No, where am I?"};
+int choice = JOptionPane.showOptionDialog(null, "Do you know where you are?", "?????", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+if (choice == 0) {
+	JOptionPane.showMessageDialog(null, "I am going to eat you! \n		You Died!");
+	
+	
+}
+
+if (choice == 1) {
+	String [] options2 = {"Deal", "No thanks."};
+choice	= JOptionPane.showOptionDialog(null, "I will ask you one question, and I will summon you back to your house if get the right answer, deal?", "?????", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options2, options2[0]);
+
+	if (choice == 0) {
+		JOptionPane.showOptionDialog(null, "What is better? Gummy sharks or gummy worms?", "?????",JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+	}
+	if (choice == 1) {
+		JOptionPane.showMessageDialog(null, "I guess I have to eat you now... \n	You Died!");
+	}
+	
+}
+
+
+
+}
+}
