a70cb8542e1ceb1f9f214330d5b5a3fe89aadb31
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-10 13:20:33 -0700

    simple sorter not finished

--- src/SimpleSorter.java
+++ src/SimpleSorter.java
@@ -1,4 +1,17 @@
+import javax.swing.JOptionPane;
 
 public class SimpleSorter {
-
+public static void main(String[] args) {
+	
+	String num = JOptionPane.showInputDialog(null, "Give me three numbers. Seperate the numbers with commas. For example, 3, 5, 9");
+	System.out.println();
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
