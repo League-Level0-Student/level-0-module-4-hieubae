4a30308d9835c06e4693cdfc0c5acaeccfc1e624
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-17 13:05:32 -0700

    Simple Sorter finished

--- src/SimpleSorter.java
+++ src/SimpleSorter.java
@@ -1,17 +1,26 @@
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class SimpleSorter {
-public static void main(String[] args) {
-	
-	String num = JOptionPane.showInputDialog(null, "Give me three numbers. Seperate the numbers with commas. For example, 3, 5, 9");
-	System.out.println();
-	
-	
-	
-	
-	
-	
-	
-	
-}
+	public static void main(String[] args) {
+
+		String num = JOptionPane.showInputDialog(null, "Give me three numbers. Seperate the numbers with spaces. For example, 3 5 9");
+		String[] a = num.split(" ");
+		int[] b = new int[3];
+		b[0] = Integer.parseInt(a[0]);
+		b[1] = Integer.parseInt(a[1]);
+		b[2] = Integer.parseInt(a[2]);
+		
+		Arrays.sort(b);
+		System.out.println(Arrays.toString(b));
+		
+		int[] c = new int[3];
+		c[2] = b[0];
+		c[1] = b[1];
+		c[0] = b[2];
+		System.out.println(Arrays.toString(c));
+		
+		
+	}
 }
