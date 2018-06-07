ce59cab9bbf5c6b8ddc3a73d7d81e777e35d0cb2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-07 13:20:36 -0700

    SleepyHead finished

--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -16,12 +16,43 @@ public class SleepyHead {
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-    
-
-
+    int ans = JOptionPane.showConfirmDialog(null, "is it a weekday?", "",JOptionPane.YES_NO_OPTION);
+        
+        if(ans==0) {
+        isWeekday = true;
+  
+        }
+        else {
+        isWeekday = false;
+        	
+        }
+     int ans2 = JOptionPane.showConfirmDialog(null, "is it a vacation?", "",JOptionPane.YES_NO_OPTION);
+        
+     if(ans2==0) {
+         isVacation = true;
+      
+         }
+         else {
+         isVacation = false;
+         	
+         }
+     if(ans2==0 || isWeekday == false) {
+    	 System.out.println("sleep in");
+    	 
+     }
+     else if (isWeekday == true && isVacation == true) {
+    	 System.out.println("sleep in");
+    	 
+     }
+     
+     else {
+    	 System.out.println("get up lazybones!");
+    	 
+     }
+     
         /*
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
+         * print “get up lazybones!” If it is a weekday, and if we are on vacation,
          * print “sleep in”.
          */
     }
